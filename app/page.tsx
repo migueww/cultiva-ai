2d247ff8349d7aa7124d76d3018e08f8b4661fa7
Miguel <miguew.png@gmail.com>
2024-12-13 14:19:15 -0300

    remove theme button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as React from "react";
-import { ButtonThemeToggleDropdown } from "@/components/theme-toggle";
 
 import {
   Card,
@@ -13,7 +12,6 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <ButtonThemeToggleDropdown />
         <Card className="w-[350px]">
           <PolygonalChart/>
         </Card>
